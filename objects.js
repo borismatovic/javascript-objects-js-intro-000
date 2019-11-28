57d953343e85fdec34575cea32f3ec28702bd4bc
confident-history-3187 <boris.matovic@gmail.com>
2019-11-28 15:49:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,7 +5,9 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {[artistName]: songTitle})
+  playlist.artistName = songTitle;
+  return playlist;
+  //return Object.assign({}, playlist, {[artistName]: songTitle})
 }
 
 function removeFromPlaylist(playlist, artistName){
